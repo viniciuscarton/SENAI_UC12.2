64a6c9e86e31209f497b50fc5a9a226e24cfdfbf
viniciuscarton <viniciuscarton@outlook.com>
2022-10-25 01:19:12 -0300

    encontro remoto 4

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,4 +1,6 @@
 using UC12_CLAB.Interfaces;
+using System.Text.RegularExpressions;
+
 namespace UC12_CLAB.Classes;
 
     public class PessoaJuridica: Pessoa, IPessoaJuridica
@@ -13,6 +15,22 @@ namespace UC12_CLAB.Classes;
 
         public bool ValidarCNPJ(string cnpj)
         {
-            throw new NotImplementedException();
+            if (Regex.IsMatch(cnpj,@"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)")){
+                if(cnpj.Length == 18)
+                {
+                    if(cnpj.Substring(11,4) == "9012")
+                    {
+                        return true;
+                    }
+                }
+                else if (cnpj.Length == 14)
+                {
+                    if (cnpj.Substring(8,4) == "9012")
+                    {
+                        return true;
+                    }
+                }     
+            }
+            return false;
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿using  UC12_CLAB.Classes;
 
 
+PessoaFisica metodopf = new PessoaFisica();
 PessoaFisica pf1 = new PessoaFisica();
 Endereco novoEnd = new Endereco();
-PessoaFisica metodopf = new PessoaFisica();
 
 pf1.Nome = "Marcelo";
 pf1.cpf = "123.456.789.01";
@@ -14,18 +14,42 @@ novoEnd.logradouro = "Rua Machado de Assis";
 novoEnd.numero = 1000;
 novoEnd.complemento = "Cemitério";
 novoEnd.endComercial = true;
-
 pf1.Endereco = novoEnd;
 
 //Console.WriteLine("Nome: " + pf1.Nome + "\nCPF: " + pf1.cpf + "\nData de nascimento: " );
 
+PessoaJuridica metodopj = new PessoaJuridica();
+PessoaJuridica pj1 = new PessoaJuridica();
+//Endereco novoEnd = new Endereco();
+
+pj1.Nome = "Fábrica de Chocolates";
+pj1.cnpj = "12.345.678/9012-34";
+pj1.razaoSoc = "Willy Wonka LTDA";
+pj1.rendimento = 8000;
+
+/*
+novoEnd.logradouro = "Rua 30 de Fevereiro";
+novoEnd.numero = 1322;
+novoEnd.complemento = "Perto de Ratanabá";
+novoEnd.endComercial = true;
+*/
+
 Console.WriteLine(@$"
+Pessoa física
 Nome: {pf1.Nome}
 CPF: {pf1.cpf}
 Data de nascimento: {pf1.dataNascimento}
 Maior de idade: {metodopf.ValidarDataNascimento(pf1.dataNascimento)}
 Endereço: {pf1.Endereco.logradouro}, {pf1.Endereco.numero}
+
+Pessoa jurídica
+Nome: {pj1.Nome}
+Razão social: {pj1.razaoSoc}
+CNPJ: {pj1.cnpj}
+CNPJ válido: {metodopj.ValidarCNPJ(pj1.cnpj)}
 ");
+
+
 
 
 
